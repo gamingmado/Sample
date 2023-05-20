82e90d5124fb475e9f39cfbb6d4978c9be9db943
lee <132149123+gamingmado@users.noreply.github.com>
2023-05-20 22:58:17 +0900

    Add window class sample.

---
+++ Sample004/Source/Window.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+class Window
+{
+public:
+	Window(HINSTANCE hInstance, int nCmdShow);
+	virtual ~Window();
+
+	int Run();
+
+private:
+	void Initialize(HINSTANCE hInstance, int nCmdShow);
+};
