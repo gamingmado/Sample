59f0844d369b1ab104f26f3579f9b25e251227de
lee <132149123+gamingmado@users.noreply.github.com>
2023-05-25 23:33:54 +0900

    Add Sample simple polygon display.

---
+++ Sample006/Source/Precompiled.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <windows.h>
+
+#include <array>
+#include <stdexcept>
+#include <string>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <wrl.h>
+#include <DirectXMath.h>
