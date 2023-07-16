3229455a8cba349a4df500914f9c263002d845d2
lee <132149123+gamingmado@users.noreply.github.com>
2023-07-16 15:14:16 +0900

    Add WindowsBasicProjectProperty files.

---
+++ WindowsBasicProjectProperty/Source/Main.cpp
@@ -0,0 +1,11 @@
+﻿#include <windows.h>
+
+int WINAPI wWinMain(
+    _In_ HINSTANCE,
+    _In_opt_ HINSTANCE,
+    _In_ LPWSTR,
+    _In_ int)
+{
+    MessageBox(nullptr, L"Hello World!", L"Title", MB_OK);
+    return 0;
+}
