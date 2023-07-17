102a09f9e64b4b548fcbe0290e9ac01c33fb560d
lee <132149123+gamingmado@users.noreply.github.com>
2023-07-17 22:39:17 +0900

    Delete unnecessary folders.

--- Sample002/Source/FormatMessage.h
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-
-std::string Format(HRESULT hr, const char* sourceFile, int sourceLine, const char* functionName);
-
-#define FORMAT_MESSAGE(hr) \
-    Format(hr, __FILE__, __LINE__, __FUNCSIG__)
