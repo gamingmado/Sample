cd21520ebd1acfc2876e94118e09083cedb4ea43
lee <132149123+gamingmado@users.noreply.github.com>
2023-07-17 18:00:03 +0900

    Add precompiled header sample.

---
+++ Windows/00003/Source/Exception.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <Windows.h>
+
+#include <stdexcept>
+
+namespace Application
+{
+
+class Exception : public std::exception
+{
+public:
+    explicit Exception(const char* message);
+};
+
+const char* FormatError(HRESULT hr, const char* sourceFile, int sourceLine, const char* functionName);
+
+} // namespace Application
+
+#define GM_FORMAT_ERROR(hr) Application::FormatError(hr, __FILE__, __LINE__, __FUNCSIG__)
