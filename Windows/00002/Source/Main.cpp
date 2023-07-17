eb8e8bdbb93dc6b9ed7eff20d4dcc426ca1d1904
lee <132149123+gamingmado@users.noreply.github.com>
2023-07-17 15:58:28 +0900

    Add exception sample.

---
+++ Windows/00002/Source/Main.cpp
@@ -0,0 +1,31 @@
+﻿#include <Windows.h>
+
+#include "Exception.h"
+
+int WINAPI wWinMain(
+    _In_ HINSTANCE,
+    _In_opt_ HINSTANCE,
+    _In_ LPWSTR,
+    _In_ int)
+{
+    int returnCode = EXIT_FAILURE;
+    try
+    {
+        bool isSuccess = true;
+        if (!isSuccess)
+        {
+            throw Application::Exception(GM_FORMAT_ERROR(E_FAIL));
+        }
+        returnCode = EXIT_SUCCESS;
+    }
+    catch (const std::exception& e)
+    {
+        OutputDebugStringA(e.what());
+    }
+    catch (...)
+    {
+        OutputDebugStringA("An unknown error has occurred.\n");
+    }
+
+    return returnCode;
+}
