603560874369d342148c5f37e5222c6a64d381bc
lee <132149123+gamingmado@users.noreply.github.com>
2023-07-30 16:33:20 +0900

    Remove DirectX12 samples to rewrite.

--- Sample007/PixelShader.hlsl
+++
@@ -1,9 +0,0 @@
-#include "ShaderHeader.hlsli"
-
-Texture2D<float4> tex : register(t0);
-SamplerState samp : register(s0);
-
-float4 main(Output output) : SV_TARGET
-{
-	return float4(tex.Sample(samp, output.uv));
-}
--- Sample007/VertexShader.hlsl
+++
@@ -1,9 +0,0 @@
-#include "ShaderHeader.hlsli"
-
-Output main(float4 pos : POSITION, float2 uv : TEXCOORD)
-{
-	Output output;
-	output.position = pos;
-	output.uv = uv;
-	return output;
-}
